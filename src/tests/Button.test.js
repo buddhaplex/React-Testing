c34a6e6890a5cd2274f02eebff734ffecd8d7870
Eric Andrade <eric.andrade@gmail.com>
2018-05-24 14:19:35 -0400

    incomplete.

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -7,9 +7,29 @@ import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+// describe('<Button />', () => {
+//   it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<Button />, div);
+//   });
+// });
+
 describe('<Button />', () => {
+  const wrapper = shallow(<Button clickHandler={() => {console.log('hello world')}}/>);
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it ('should return one child node, which is the text node.', () => {
+    expect(wrapper).toHaveLength(1);
+  })
+  it ('test', () => {
+    console.log(wrapper.find('button').simulate('click'))
+  })
+  it('check snapshot',()=>{
+
+    let element = shallow(<Button/>)
+    expect(element).toMatchSnapshot()
+
+  })
 });
